b55afb16e01da9877fd329b5334fb407934c38c4
Flatiron School <email.the.anderson@gmail.com>
2019-07-08 16:04:32 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,24 @@
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require 'json'
+
+# welcome
+# character = get_character_from_user
+# show_character_movies(character)
+
+require 'rest-client'
+character_data = RestClient.get("https://swapi.co/api/people/1")
+parsed_data = JSON.parse(character_data)
+
+# puts character_data
+# puts parsed_data
+# binding.pry
+# "This is the end, my friends!"
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+get_character_movies_from_api(character)
+
+
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,43 @@
+
+
+
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome! Please select a character from the epic space opera, Star Wars!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  character = gets.chomp 
+  character
 end
+
+def get_results_array
+  i = 1
+  final_hash = []
+  while i < 10 
+    response_string = RestClient.get("https://swapi.co/api/people/?page=#{i}")
+    response_hash = JSON.parse(response_string)
+    final_hash = final_hash.concat(response_hash["results"])
+    i += 1
+  end
+  final_hash
+end
+# can be used to parse data from multiple pages
+
+def get_character_movies_from_api(name)
+  parsed_data = get_results_array
+  films = []
+  parsed_data.map do |result_instance|
+      if result_instance["name"] == name 
+          # puts result_instance["name"]
+          films = result_instance["films"]
+      end
+   end
+  film_array = []
+  films.map do |film_instance|
+    film_data = RestClient.get(film_instance)
+    parsed_film = JSON.parse(film_data)
+    film_array << parsed_film
+  end 
+  puts film_array
+
+end 
