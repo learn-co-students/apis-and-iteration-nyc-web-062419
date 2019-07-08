fb13691cfc6b0d85f38f87c6e435c292c223a7d7
IanGrubb <yangrubb@gmail.com>
2019-07-08 17:32:24 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,75 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def find_char_by_name(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
+  #parse request to hash
   response_hash = JSON.parse(response_string)
+  #find the character
+  character = response_hash["results"].find{|character_hash| character_hash["name"].downcase == character_name}
+  character
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def find_movie_by_name(movie_name) 
+  #make the web request
+  response_string = RestClient.get('https://www.swapi.co/api/films/')
+  #parse request to hash
+  response_hash = JSON.parse(response_string)
+  #find the film
+  film = response_hash["results"].find{|movie_hash| movie_hash["title"].downcase == movie_name}
+  film
+end
+
+def total_mass_of_cast(film)
+  #bring in movie hash
+  #grab all char urls
+  character_urls = film["characters"]
+
+  mass = character_urls.map do |char_url|
+    char_hash = JSON.parse(RestClient.get(char_url))
+    char_hash["mass"].to_i
+  end.sum
+  
+  mass
+
+end
+
+def order_films_by_mass
+  films_hash = JSON.parse(RestClient.get('https://www.swapi.co/api/films/'))
+  films_array = films_hash["results"]
+  
+  array1 = films_array.map do |film|
+    title = film["title"]
+    
+    mass = total_mass_of_cast(film)
+    {title: title, mass: mass}
+  end
+
+  array2 = array1.sort_by{|pair| pair[:mass]}
+  
+  output = array2.map do |name_weight_combo|
+    "#{name_weight_combo[:title]} weighs #{name_weight_combo[:mass]}Lbs"
+  end
+
+  puts output
+end
+
+
+
+def get_character_movies_from_api(character_name)
+  #call on helper method to find char
+  character = find_char_by_name(character_name)
+  #grab char films
+  char_films = character["films"]
+  #storing list of each movies json
+  char_film_responses = char_films.collect{|url| JSON.parse(RestClient.get(url))}
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  movie_names = films.map {|film| film["title"]}
+  puts movie_names
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name:"
   # use gets to capture the user's input. This method should return that input, downcased.
+  return gets.chomp.downcase
+
+end
+
+def get_film_from_user
+  puts "Please enter a film name:"
+  # use gets to capture the user's input. This method should return that input, downcased.
+  return gets.chomp.downcase
 end
