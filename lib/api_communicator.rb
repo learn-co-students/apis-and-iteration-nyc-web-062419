6ea598a7d7cff2ae1368cd5a89e9e87bb2fe0d0d
github username <github email address>
2019-07-08 14:25:05 -0400

    finished

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,16 +2,14 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def connect_to_people_api
+def people_api
   response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  response_hash = JSON.parse(response_string)["results"]
 end
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  character_films = response_hash["results"].find {|character| character["name"] == character_name}["films"]
+  character_films = people_api.find {|character| character["name"] == character_name}["films"]
   character_films.map do |api_link|
     response_string = RestClient.get(api_link)
     response_hash = JSON.parse(response_string)
